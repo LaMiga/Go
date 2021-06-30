e22506c9dac4e3c2dcfa8bb75005b3f3e15cf5bb
LaMiga <milual1988@gmail.com>
2021-06-30 18:11:51 +0200

    añadido un  handler para yamls

--- handler.go
+++ handler.go
@@ -2,19 +2,18 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 /*
 	Devuelve un http.HandlerFunc que mapea cualquier dirección con
-	su URL
+	su URL. Si tiene una dirección válida, nos vamos a ella
+	si no, usamos el fallback
 */
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		/*
-			Si tiene una dirección válida, nos vamos a ella
-			si no, usamos el fallback
-		*/
 		path := r.URL.Path
 
 		if dest, ok := pathsToUrls[path]; ok {
@@ -23,4 +22,35 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 		}
 		fallback.ServeHTTP(w, r)
 	}
+}
+
+func YAMLhandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
